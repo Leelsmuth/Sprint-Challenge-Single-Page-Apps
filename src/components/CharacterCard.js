66cadc57f61473428f542008e58f3720d738624b
Leelsmuth <femianjorin@rocketmail.com>
2019-09-20 11:14:15 +0100

    data rendered to screen, working on search form

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div>
+      <h4> {`Name: ${props.name}`} </h4>
+      <img src={props.image} alt="#" />
+      <h4> {`Species: ${props.species}`} </h4>
+      <h4> {`Status: ${props.status}`} </h4>
+      <h4> {`Origin: ${props.origin}`} </h4>
+      <h4> {`Location: ${props.location}`} </h4>
+    </div>
+  );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+// import React, { useState, useEffect } from "react";
 
-export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
-}
+// export default function SearchForm() {
+//   return (
+//     <div className="search-form">
+//       <label>
+//         <label for="name">Search:</label>
+//         <input
+//           id="name"
+//           type="text"
+//           name="textfield"
+//           placeholder="Search"
+//           // value={searchTerm}
+//           // onChange={handleChange}
+//         />
+//       </label>
+//     </div>
+//   );
+// }
